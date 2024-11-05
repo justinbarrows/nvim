b8ae491563d0833dd622322ff6db02e9bcd33856
justinbarrows <jbarrows1030@gmail.com>
2024-11-05 11:15:57 -0500

    colors.lua from theprimagen

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,40 +1,89 @@
+function ColorMyPencils(color)
+  color = color or 'rose-pine-moon'
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+end
+
 return {
-  'shaunsingh/nord.nvim',
-  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    -- Example config in lua
-    vim.g.nord_contrast = true                   -- Make sidebars and popup menus like nvim-tree and telescope have a different background
-    vim.g.nord_borders = false                   -- Enable the border between verticaly split windows visable
-    vim.g.nord_disable_background = true         -- Disable the setting of background color so that NeoVim can use your terminal background
-    vim.g.set_cursorline_transparent = false     -- Set the cursorline transparent/visible
-    vim.g.nord_italic = false                    -- enables/disables italics
-    vim.g.nord_enable_sidebar_background = false -- Re-enables the background of the sidebar if you disabled the background of everything
-    vim.g.nord_uniform_diff_background = true    -- enables/disables colorful backgrounds when used in diff mode
-    vim.g.nord_bold = false                      -- enables/disables bold
 
-    -- Load the colorscheme
-    require('nord').set()
+  {
+    'erikbackman/brightburn.vim',
+  },
 
-    -- Function to set menu borders to transparent
-    -- local set_menu_border_transparency = function()
-    --   vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', fg = 'NONE' })
-    --   vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'NONE', fg = 'NONE' })
-    -- end
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    opts = {},
+    config = function()
+      ColorMyPencils()
+    end,
+  },
+  {
+    'ellisonleao/gruvbox.nvim',
+    name = 'gruvbox',
+    config = function()
+      require('gruvbox').setup {
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = false,
+        bold = true,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = '',  -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      }
+    end,
+  },
+  {
+    'folke/tokyonight.nvim',
+    config = function()
+      require('tokyonight').setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = 'night',        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = true,     -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = false },
+          keywords = { italic = false },
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = 'dark', -- style for sidebars, see below
+          floats = 'dark',   -- style for floating windows
+        },
+      }
+    end,
+  },
 
-    -- Execute the function once after loading the colorscheme
-    -- set_menu_border_transparency()
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      require('rose-pine').setup {
+        disable_background = true,
+        styles = {
+          italic = false,
+        },
+      }
 
-    local bg_transparent = true
-
-    -- Toggle background transparency
-    local toggle_transparency = function()
-      bg_transparent = not bg_transparent
-      vim.g.nord_disable_background = bg_transparent
-      vim.cmd [[colorscheme nord]]
-      -- set_menu_border_transparency()
-    end
-
-    vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
-  end,
+      ColorMyPencils()
+    end,
+  },
 }
